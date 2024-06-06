edcae23bbc271818cb18d499f488fbde3de96fb5
Lenagran18 <Lenagran18@gmail.com>
2024-06-06 17:12:59 -0400

    Register - created fetchData function

---
+++ client/src/main.js
@@ -0,0 +1,26 @@
+//Functions useful in multiple pages
+// will add the route http.../user/register
+// data - {"username": "Lena", "password": "123"}
+//methodType - "POST", "PUT", "DELETE"
+
+export async function fetchData(route = '', data = {}, methodType) {
+    //TEST
+    console.log(`URL: http://localhost:4000${route}`);
+    console.log('Data:', data);
+    console.log('Method:', methodType);
+    //sending over our data to specified route in server
+    const response = await fetch(`http://localhost:4000${route}`, {
+        method: methodType,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    //dealing with our response from server
+    if (response.ok) {
+        return await response.json(); //If it's okay
+    } else {
+        throw await response.json(); //If not, throw error 
+    }
+}
