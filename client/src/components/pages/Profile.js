65acff85a20a912432749093a915c4afd24e180d
Lenagran18 <Lenagran18@gmail.com>
2024-07-03 17:50:03 -0400

    Display username on the profile page

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -10,72 +10,65 @@ const Profile = () => {
         authorId: authorId,
         postContent: ''
     });
-
     const [posts, setPosts] = useState([]);
-
-   
-
     const { postContent } = post
-   
+    const onChange = (e) => setPost({ ...post, [e.target.name]: e.target.value })
 
-    const onChange = (e) => setPost({ ...post, [e.target.name]: e.target.value }) 
     const onSubmit = (e) => {
-  
-            e.preventDefault();
-            console.log('authorId:', post.authorId)
-            fetchData('/post/createPost', 
-            { 
+
+        e.preventDefault();
+        console.log('authorId:', post.authorId)
+        fetchData('/post/createPost',
+            {
                 postContent,
                 authorId: authorId
-            }, 
+            },
             "POST")
 
             .then((data) => {
                 console.log(data);
-                if(!data.message) {
-                   
-                    setPosts([ postContent, ...posts ]); //Add new post
+                if (!data.message) {
+
+                    setPosts([postContent, ...posts]); //Add new post
                     setPost({ postContent: '' }); //Clear text area
                 }
             })
             .catch((error) => {
                 console.log(error)
             })
-        // }   
-    }   
+    }
 
-    return(
-
-            <div className="writePost">
-                <div className="mb-3">
-                    <form onSubmit={onSubmit}>
-                    <h1>Profile</h1>
+    return (
+        <div className="writePost">
+            <div className="mb-3">
+                <form onSubmit={onSubmit}>
+                    <h1> {user.username}'s Profile</h1>
 
                     <label htmlFor="floatingTextarea"> Create Post</label>
                     <textarea
                         className="form-control"
-                        placeholder="What's new..." 
+                        placeholder="What's new..."
                         id="postContent"
                         name="postContent"
                         onChange={onChange}
                         value={postContent}
                         required
-                    /> 
-                <button type="submit" className="btn">Submit</button>
-            </form>
-        </div>
+                    />
+                    <button type="submit" className="btn">Post</button>
+                </form>
+            </div>
+
             <div className="posts">
-            {/* <h2>`${username}'s Posts`</h2> */}
-            {posts.length !== 0 ? (
-                <ul>
-                    {posts.map((content, index) => (
-                        <li key={index} className="post">
-                            {content}
-                        </li>
-                    ))}
-                </ul>
+                {posts.length !== 0 ? (
+                    <ul>
+                        {posts.map((content, index) => (
+                            <li key={index} className="posts">
+                                {content}
+                            </li>
+                        ))}
+                    </ul>
                 ) : (<p>No posts yet</p>)
-            }
+                }
             </div>
         </div>
     );
